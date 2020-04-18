33fe409bd6877f8e1226470c16e1677c0424ae2b
ally <alwyncrasta000@gmail.com>
2020-04-19 00:34:30 +0530

    scm

--- app/MajorProgrammes.php
+++ app/MajorProgrammes.php
@@ -7,4 +7,10 @@ use Illuminate\Database\Eloquent\Model;
 class MajorProgrammes extends Model
 {
     protected $fillable = ['from','to','level','userId','noOfBeneficiaries','department','programme','facultyAssociation','desc'];
+
+
+
 }
+
+
+
